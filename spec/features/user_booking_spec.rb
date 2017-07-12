3e98cd2acbf77c469b41addbb86addf0b80b2d98
jinimcoroneo <jinim1983@gmail.com>
2017-07-12 14:54:54 +0100

    add drop down calendar

--- spec/features/user_booking_spec.rb
+++ spec/features/user_booking_spec.rb
@@ -3,6 +3,6 @@ feature "Booking" do
     sign_up(email: "johnsmith@aol.com", first_name: "John", last_name: "Smith", username: "jsmith", password: "password", password_confirmation: "password")
     new_listing
     fill_in 'location', with: 'London'
-    expect(page).to have_select('start-date', selected: '12/07/2017')
+    expect(page).to have_content('Check-in')
   end
 end
